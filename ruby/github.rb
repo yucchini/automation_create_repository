aad67b3a581e34f96689eceec36ac688d4e3a3d3
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2020-06-07 12:08:38 +0900

    Re: bundle

--- ruby/github.rb
+++ ruby/github.rb
@@ -1,4 +1,14 @@
 require 'octokit'
 require 'dotenv'
 
-@name = 
+Dotenv.load '.env'
+
+username =  ENV['USERNAME']
+password =  ENV['PASSWORD']
+github_access_token =  ENV['GITHUB_ACCESS_TOKEN']
+filepath = ENV['FILEPATH']
+
+
+# client = Octokit::Client.new(login: username, password: password)
+client = Octokit::Client.new(access_token: github_access_token)
+puts client.user.name
