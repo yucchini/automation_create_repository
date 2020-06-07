dbe77b77a8a316c126181a838d4b592de0374a86
Yuki Shimizu <yucchini.yuki1014@gmail.com>
2020-06-07 14:56:31 +0900

    Remove unnecessary code

--- github.rb
+++ github.rb
@@ -3,16 +3,13 @@ require 'dotenv'
 
 Dotenv.load '.env'
 
-username =  ENV['USERNAME']
-password =  ENV['PASSWORD']
 github_access_token =  ENV['GITHUB_ACCESS_TOKEN']
 path = ENV['FILEPATH']
 
 system('cd')
-folderName = ARGV[0].to_s
+folder_name = ARGV[0].to_s
 Dir.mkdir(path + folderName)
 client = Octokit::Client.new(access_token: github_access_token)
 client.user.login
-client.create_repository(folderName)
-puts "Succesfully created repository #{folderName}"
-# client.delete_repository(folderName)
+client.create_repository(folder_name)
+puts "Succesfully created repository #{folder_name}"
